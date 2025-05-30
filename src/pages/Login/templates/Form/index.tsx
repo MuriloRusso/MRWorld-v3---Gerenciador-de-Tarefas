2b895d41326ed0dd8da030c46a5610b03a310d02
Murilo Henrique Russo <79058778+MuriloRusso@users.noreply.github.com>
2025-05-30 18:19:56 -0300

    wip

--- src/pages/Login/templates/Form/index.tsx
+++ src/pages/Login/templates/Form/index.tsx
@@ -1,9 +1,10 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import { inputProps } from "../../../../types/input.js";
 import { LoginFields } from "../../../../types/login.js";
 import InputUser from "../../components/InputUser/index";
 import InputPassword from "../../components/InputPassword/index";
 import ButtonSubmit from "../../components/ButtonSubmit/index";
+import { Link } from "react-router-dom";
 
 type FormProps = {
     userField: inputProps;
@@ -18,6 +19,13 @@ export default function Form ({userField, passwordField, handleLoginFieldChange,
             <InputUser userField={userField} handleLoginFieldChange={handleLoginFieldChange} />
             <InputPassword passwordField={passwordField} handleLoginFieldChange={handleLoginFieldChange} />
             <ButtonSubmit />
+            <Typography
+                variant="inherit"
+                component="p"
+                sx={{color: "#828282", textAlign: "center"}}
+            >
+                Desenvolvido por <Link to="https://murilorusso.com.br/" style={{color: "black", fontWeight: 600}} target="_blank">Murilo Russo</Link>
+            </Typography>
         </Box>
     )
 }
