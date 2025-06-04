b69338dc7edd38d2487c3cdebf58caaf9d65f6bb
Murilo Henrique Russo <79058778+MuriloRusso@users.noreply.github.com>
2025-06-04 18:38:10 -0300

    inclued skeleton

--- src/pages/Clients/templates/Container/index.tsx
+++ src/pages/Clients/templates/Container/index.tsx
@@ -1,9 +1,19 @@
-import { Box, Grid, Typography } from "@mui/material";
+import { Box, Grid, Skeleton, Typography } from "@mui/material";
 import ButtonNew from "../../components/ButtonNew";
 import List from "../List";
 import Search from "../Search";
+import { useEffect, useState } from "react";
 
 export default function Container({handleModal}:{handleModal: (value:boolean) => void;}) {
+
+    const [ loading, setLoading ] = useState<boolean>(true);
+
+    useEffect(()=>{
+        setTimeout(()=>{
+            setLoading(false)
+        }, 2000)
+    }, [])
+
     return (
         <Box
             className="container" 
@@ -17,11 +27,14 @@ export default function Container({handleModal}:{handleModal: (value:boolean) =>
             }}
         >
             <Grid sx={{display: 'flex', flexDirection: 'row', justifyContent: 'flex-end', marginY: 5}}>
-                <ButtonNew handleModal={handleModal}/>
+                {/* <ButtonNew handleModal={handleModal}/> */}
+                {loading ? <Skeleton variant="rounded" width={170} height={40} /> : <ButtonNew handleModal={handleModal}/>}
             </Grid>
             <Typography variant="h2" component="h2" sx={{fontSize: 25}}>Empresas/Clientes</Typography>
-            <Search/>
-            <List/>
+            
+            {loading ? <Skeleton variant="rounded" width={450} height={90} /> : <Search/>}
+
+            {loading ? <Skeleton variant="rounded" width={'100%'} height={'60vh'} /> : <List/>}
         </Box>
     )
 }
