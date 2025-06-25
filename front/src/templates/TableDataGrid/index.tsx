d0dbe1768ebec73febcdd13b626550e61da4f320
Murilo Henrique Russo <79058778+MuriloRusso@users.noreply.github.com>
2025-06-25 18:57:36 -0300

    wip

--- front/src/templates/TableDataGrid/index.tsx
+++ front/src/templates/TableDataGrid/index.tsx
@@ -15,69 +15,71 @@ export default function TableDataGrid({handleModal, rows, columns, sx}:TableData
 
   return (
     <Paper sx={sx}>
-      <Grid sx={{display: 'flex', justifyContent: "flex-end", padding: 2}}>
-        <ButtonNew handleModal={handleModal}/>
-      </Grid>
-      <Divider/>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        initialState={{ pagination: { paginationModel } }}
-        pageSizeOptions={[5, 10, 20]}
-        checkboxSelection
-        localeText={
-            {
-                noRowsLabel: 'Sem linhas',
-                columnMenuHideColumn: "Ocultar Coluna",
-                columnsManagementNoColumns: "Gerenciar Colunas",
-                columnMenuSortAsc: "Crescente",
-                columnMenuSortDesc: "Decrescente",
-                columnMenuUnsort: "Desordenar",
-                columnMenuFilter: "Filtro",
-                columnHeaderSortIconLabel: "Organizar",
-                filterOperatorContains: "Contém",
-                filterOperatorDoesNotContain: "Não Contem",
-                filterOperatorEquals: "Igual",
-                filterOperatorDoesNotEqual: "Não é igual",
-                filterOperatorStartsWith: "Começa com",
-                filterOperatorEndsWith: "Termina com",
-                filterOperatorIsEmpty: "Está vázio",
-                filterOperatorIsNotEmpty: "Não está vázio",
-                filterOperatorIsAnyOf: "Qualquer um",
-                columnMenuShowColumns: "Mostar Coluna",
-                columnsManagementReset: "Resetar",
-                columnsManagementShowHideAllText: "Ocultar/Exibir",
-                columnsManagementSearchTitle: "Buscar",
-                filterPanelOperator: "Operador",
-                filterPanelColumns: "Coluna",
-                filterPanelInputLabel: "Valor",
-                filterPanelInputPlaceholder: "Valor do filtro",
-                paginationRowsPerPage: "Itens por página",
-                paginationDisplayedRows(params) {
-                    return params.from + '-' + params.to + ' de ' + params.count
-                },
-                footerRowSelected(params) {
-                    return  params + " Itens selecionados"
-                },
-                paginationItemAriaLabel(type) {
-                    switch (type) {
-                        case 'next':
-                        return 'Próxima Página';
-                        case 'previous':
-                        return 'Página Anterior';
-                        case 'first':
-                        return 'Primeira Página';
-                        case 'last':
-                        return 'Última Página';
-                        default:
-                        return `Página ${type}`;
-                    }
-                }
+      <Grid sx={{display: 'flex', flexDirection: 'column', height: "100%"}}>
+        <Grid sx={{display: 'flex', justifyContent: "flex-end", padding: 2}}>
+          <ButtonNew handleModal={handleModal}/>
+        </Grid>
+        <Divider/>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          initialState={{ pagination: { paginationModel } }}
+          pageSizeOptions={[5, 10, 20]}
+          checkboxSelection
+          localeText={
+              {
+                  noRowsLabel: 'Sem linhas',
+                  columnMenuHideColumn: "Ocultar Coluna",
+                  columnsManagementNoColumns: "Gerenciar Colunas",
+                  columnMenuSortAsc: "Crescente",
+                  columnMenuSortDesc: "Decrescente",
+                  columnMenuUnsort: "Desordenar",
+                  columnMenuFilter: "Filtro",
+                  columnHeaderSortIconLabel: "Organizar",
+                  filterOperatorContains: "Contém",
+                  filterOperatorDoesNotContain: "Não Contem",
+                  filterOperatorEquals: "Igual",
+                  filterOperatorDoesNotEqual: "Não é igual",
+                  filterOperatorStartsWith: "Começa com",
+                  filterOperatorEndsWith: "Termina com",
+                  filterOperatorIsEmpty: "Está vázio",
+                  filterOperatorIsNotEmpty: "Não está vázio",
+                  filterOperatorIsAnyOf: "Qualquer um",
+                  columnMenuShowColumns: "Mostar Coluna",
+                  columnsManagementReset: "Resetar",
+                  columnsManagementShowHideAllText: "Ocultar/Exibir",
+                  columnsManagementSearchTitle: "Buscar",
+                  filterPanelOperator: "Operador",
+                  filterPanelColumns: "Coluna",
+                  filterPanelInputLabel: "Valor",
+                  filterPanelInputPlaceholder: "Valor do filtro",
+                  paginationRowsPerPage: "Itens por página",
+                  paginationDisplayedRows(params) {
+                      return params.from + '-' + params.to + ' de ' + params.count
+                  },
+                  footerRowSelected(params) {
+                      return  params + " Itens selecionados"
+                  },
+                  paginationItemAriaLabel(type) {
+                      switch (type) {
+                          case 'next':
+                          return 'Próxima Página';
+                          case 'previous':
+                          return 'Página Anterior';
+                          case 'first':
+                          return 'Primeira Página';
+                          case 'last':
+                          return 'Última Página';
+                          default:
+                          return `Página ${type}`;
+                      }
+                  }
 
-            }
-        }
-        sx={{ border: 0, maxHeight: "inherit" }}
-      />
+              }
+          }
+          sx={{ border: 0 }}
+        />
+      </Grid>
     </Paper>
   );
 }
