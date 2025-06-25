d3e3ae82ff4fad47eb4fa324ca7f967e1586c44e
Murilo Henrique Russo <79058778+MuriloRusso@users.noreply.github.com>
2025-06-25 13:03:26 -0300

    fixed person Type

--- front/src/types/person.tsx
+++ front/src/types/person.tsx
@@ -1,11 +1,13 @@
 export type Person = {
     id: number;
     name: string;
+    id_client: number;
     avatar?: string;
     phone?: string;
     email?: string;
     position?: string; //cargo
     function?: string;
+    is_owner: boolean;
     notes?: string;
     created_at: string;
     updated_at?: string;
