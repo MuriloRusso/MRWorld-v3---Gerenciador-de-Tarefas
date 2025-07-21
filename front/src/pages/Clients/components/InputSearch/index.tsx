92a8043625deecaa4afbb918888fa10f3dbc7927
Murilo Henrique Russo <79058778+MuriloRusso@users.noreply.github.com>
2025-07-21 20:45:56 -0300

    search client in progress

--- front/src/pages/Clients/components/InputSearch/index.tsx
+++ front/src/pages/Clients/components/InputSearch/index.tsx
@@ -1,7 +1,7 @@
 import Input from "../../../../components/Input";
 
-export default function InputSearch() {
+export default function InputSearch({search, handleSearch}:{search: string, handleSearch: (e: React.ChangeEvent<HTMLInputElement>) => void}) {
     return (
-        <Input value="" onChange={()=>{}} placeholder="Buscar..."/>
+        <Input value={search} onChange={() => handleSearch} placeholder="Buscar..."/>
     )
 }
--- front/src/pages/Clients/templates/Search/index.tsx
+++ front/src/pages/Clients/templates/Search/index.tsx
@@ -5,7 +5,7 @@ import ButtonSubmitSearch from "../../components/ButtonSubmitSearch";
 export default function Search({search, handleSearch}:{search: string, handleSearch: (e: React.ChangeEvent<HTMLInputElement>) => void}){
     return(
         <Grid sx={{display: 'flex', flexDirection: "row", gap: 1, maxWidth: '100%', width: 400, alignItems: 'center'}}>
-            <InputSearch/>
+            <InputSearch search={search} handleSearch={handleSearch}/>
             <ButtonSubmitSearch/>
         </Grid>
     )
