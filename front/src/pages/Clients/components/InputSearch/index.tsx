31fa0218aa90330087ef148d50e5661057c9d5e5
Murilo Henrique Russo <79058778+MuriloRusso@users.noreply.github.com>
2025-07-22 19:50:25 -0300

    wip

--- front/src/pages/Clients/components/InputSearch/index.tsx
+++ front/src/pages/Clients/components/InputSearch/index.tsx
@@ -1,7 +1,5 @@
 import Input from "../../../../components/Input";
 
 export default function InputSearch({search, handleSearch}:{search: string, handleSearch: (e: string) => void}) {
-    return (
-        <Input value={search} onChange={handleSearch} placeholder="Buscar..."/>
-    )
+    return <Input value={search} onChange={handleSearch} placeholder="Buscar..."/>
 }
